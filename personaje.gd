e7d30947a97942084344ac006b15ed0dfc7b80a4
Nahuel Lautaro Jimenez <nahuellautaro.jimenez@estudiantes.unahur.edu.ar>
2024-09-06 19:43:15 -0300

    Agregue comentarios

--- personaje.gd
+++ personaje.gd
@@ -1,17 +1,17 @@
 extends Area2D
 
-@export var speed = 400 
-var screen_size 
+@export var speed = 400 #Agregar Var de velocidad al personaje.
+var screen_size # Var del tamaño de la pantalla.
 
 
 func _ready():
-	screen_size = get_viewport_rect().size
+	screen_size = get_viewport_rect().size # Establecer el tamaño de la pantalla.
 
 
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	var velocity = Vector2.ZERO # Declarar la variable de velocidad en 0.0
 	if Input.is_action_pressed("Move_Right"):
-		velocity.x += 1
+		velocity.x += 1 ##Cambiar la velocidad a 1.1 en la direccion de la tecla.
 	if Input.is_action_pressed("Move_Left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("Move_Down"):
@@ -21,9 +21,8 @@ func _process(delta):
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
+		# Hacer la velocidad constante para que coincida con los fotogramas.
 
 	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	position = position.clamp(Vector2.ZERO, screen_size) #Hacer que el personaje no pueda pasar los limites de la pantalla.
+	
